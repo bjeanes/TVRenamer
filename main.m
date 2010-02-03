b1db2d82ce8876beddfbc8ab169bb1c6e126375c
Bodaniel Jeanes <me@bjeanes.com>
2010-02-03 18:06:04 +1000

    Initial commit

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TVRenamer
+//
+//  Created by Bodaniel Jeanes on 3/02/10.
+//  Copyright Mocra 2010. All rights reserved.
+//
+
+#import <MacRuby/MacRuby.h>
+
+int main(int argc, char *argv[])
+{
+    return macruby_main("rb_main.rb", argc, argv);
+}
