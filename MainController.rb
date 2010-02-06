74d2d3f2650e52cd8d6532943d40f34772568794
Bodaniel Jeanes <me@bjeanes.com>
2010-02-07 01:48:09 +1000

    'Rename All' button performs renames from old name to new name for all items in the list

--- MainController.rb
+++ MainController.rb
@@ -5,4 +5,13 @@
 # Copyright 2010 Mocra. All rights reserved.
 
 class MainController < NSWindowController
+  attr_accessor :file_list
+
+  def perform_rename(sender)
+    file_list.arrangedObjects.each do |episode|
+      Dir.chdir(episode.dirname) do
+        episode.rename(episode.new_name)
+      end
+    end
+  end
 end
